1f673e050c4755e8d5b0b7ebc012ce4524e0773e
DimaMirk <dima.myruk@gmail.com>
2023-10-22 18:52:15 +0300

    added ctrlWrapper

--- controls/contrl.js
+++ controls/contrl.js
@@ -1,90 +1,67 @@
 const Contact = require('../models/contact')
 const Joi = require('joi')
 
-const HttpError = require('../helpers/httpError')
+const { HttpError } = require('../helpers')
+const { CtrlWrapper } = require('../helpers')
 let objectShema = Joi.object({
     email: Joi.string().required(),
     phone: Joi.string().required(),
     name: Joi.string().required()
 })
 
-const all = async(req , res, next) => {
-     try {
-        let allContacts = await Contact.find()
-        res.json(allContacts)
-        
-    } catch(error) {
-        res.status(500).json
-            ({ message: 'Server error' })
-    }
+const all = async(req , res) => {
+    let allContacts = await Contact.find()
+    res.json(allContacts)    
 }
 
-const getById = async (req, res, next) => {
-     let id = req.params.id
-    try {
-        let contact = await Contact.findById(id)
-        if (!contact) {
-            throw HttpError(404,'Not found')
-        }
-        res.json(contact)
-        
-    } catch (error) {
-        next(error)
-    }
-}
-
-const post = async (req, res, next) => {
-    try {
-
-        const { error } = objectShema.validate(req.body)
-        if (error) {
-            throw HttpError(400,error.message)
-        }
-        const result = await Contact.create(req.body)
-        res.status(201).json(result)
-    } catch (err) {
-        next(err)
-    }
-}
-
-const put = async (req, res, next) => {
+const getById = async (req, res) => {
     let id = req.params.id
-    try {
-        const { error } = objectShema.validate(req.body)
-        if (error) {
-            throw HttpError(400,error.message)
-        }
-       
-        let result = await Contact.findByIdAndUpdate(id, req.body, { new: true })
-        if (!result) {
-            throw HttpError(404,'Not found')
-        }
-        res.json(result)
+    let contact = await Contact.findById(id)
 
-    } catch (err) {
-        next(err)
+    if (!contact) {
+        throw HttpError(404,'Not found')
     }
+    res.json(contact)
 }
 
-const deleteById = async (req, res, next) => {
-      try {
-        const { id } = req.params
-        let result =  await Contact.findByIdAndDelete(id)
-        if (!result) {
-            throw HttpError(400, error.message)
-        }
-        res.json(result)
-        
-    } catch (err) {
-        next(err)
+const post = async (req, res) => {
+    const { error } = objectShema.validate(req.body)
+    if (error) {
+        throw HttpError(400,error.message)
     }
+    const result = await Contact.create(req.body)
+    res.status(201).json(result)
+}
+
+const put = async (req, res) => {
+    let id = req.params.id
+
+    const { error } = objectShema.validate(req.body)
+    if (error) {
+        throw HttpError(400,error.message)
+    }
+       
+    let result = await Contact.findByIdAndUpdate(id, req.body, { new: true })
+    if (!result) {
+        throw HttpError(404,'Not found')
+    }
+    res.json(result)
+}
+
+const deleteById = async (req, res) => {
+    const { id } = req.params
+    let result =  await Contact.findByIdAndDelete(id)
+    if (!result) {
+        throw HttpError(400, error.message)
+    }
+    res.json(result)
 }
 
 
 module.exports = {
-    all,
-    getById,
-    post,
-    put,
-    deleteById
+    all:CtrlWrapper(all),
+    getById:CtrlWrapper(getById),
+    post:CtrlWrapper(post),
+    put:CtrlWrapper(put),
+    deleteById:CtrlWrapper(deleteById)
 }
--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,7 @@
 const HttpError = require('./httpError')
+const CtrlWrapper = require('./cntrWrapper')
 
 module.exports = {
-    HttpError
+    HttpError,
+    CtrlWrapper
 } 
